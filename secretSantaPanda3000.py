fe483dc90753b28aa7e2b20992a699bb57f0b6e0
PandaSantaBot3000 <93723509+PandaSantaBot3000@users.noreply.github.com>
2021-11-04 17:42:28 +0100

    Minor tweaks

--- secretSantaPanda3000.py
+++ secretSantaPanda3000.py
@@ -6,12 +6,12 @@ givere = ["Bob", "Alice", "Peder Aas", "Marte Kirkerud"]
 
 mottakere = givere.copy()
 
-random.shuffle(givere)        #rister i hatten med loddene
+random.shuffle(givere) #rister i hatten
 random.shuffle(mottakere)
 
 paringer = []
 
-for giv in givere: #trekker gavepar
+for giv in givere: #trekker giver-mottaker-par
     mottaker = secrets.choice(mottakere)
     while mottaker == giv:
         mottaker = secrets.choice(mottakere)
@@ -19,15 +19,15 @@ for giv in givere: #trekker gavepar
     par = [giv, mottaker]
     paringer.append(par)
 
-hemmeligOrd = "HEMMELIG"
+hemmeligOrd = "HEMMELIG_ORD" #kombineres med trukket navn og kjøres gjennom en krytografisk enveisfunksjon
 
 for i in range(len(paringer)):
     mottaker = paringer[i][1]
     mottakerHash = mottaker + hemmeligOrd
     print(paringer[i][0], "trakk: ", end="")
-    encoded = hashlib.md5(mottakerHash.encode()) #kryptografisk enveisfunsksjon som deles offentlig for etterettelighet
-    f = open(paringer[i][0] + ".txt", "w")
-    f.write("Gratulerer, " + paringer[i][0] +"! Du trakk " + mottaker + " som din hemmelige gavemottaker!\n\n")
+    encoded = hashlib.md5(mottakerHash.encode()) #Sjekksummen deles offentlig for etterettelighet
+    f = open("{0}.txt".format(paringer[i][0]), "w")
+    f.write("Gratulerer, {0}! Du trakk {1} som din hemmelige gavemottaker!\n\n".format(paringer[i][0], mottaker))
     f.write("""       
             ,,,         ,,,
           ;"   ^;     ;'   ",
@@ -42,6 +42,7 @@ for i in range(len(paringer)):
             "Y$$$"*"$$$Y"    
                "$b.$$"     """)
     f.write("\n"*2)
-    f.write("Sjekksum for din trekning (skal vaere identisk til den som ble delt offentlig): " + encoded.hexdigest())
+    f.write("Sjekksum for din trekning (skal vaere identisk til den som ble delt offentlig): {0}".format(
+        encoded.hexdigest()))
     f.close()
     print(encoded.hexdigest())
